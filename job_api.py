cf6ca8e7d1ef12125aecfa80fbccf463005e81e1
Aviraljain13 <157096933+Aviraljain13@users.noreply.github.com>
2024-02-29 09:36:37 +0530

    Update job_api.py

--- job_api.py
+++ job_api.py
@@ -4,15 +4,16 @@ import math
 app = Flask(__name__)
 
 
-@app.route('/get_job',)
-def rot():
+@app.route('/get_job',methods=['GET'])
+def info_job():
     page = request.args.get('page_num', type=int)
     loc = request.args.get('location', type=str)
-    
-    val=job_datas.job_data(loc,math.ceil(page/5))
-    num=math.floor(page/5)
-    start=(num)*5
-    end=(num+1)*5 
+    tim = request.args.get('when_generated', type=str)
+    job_loc = request.args.get('job_loc', type=str)
+    val=job_datas.job_data(loc,math.ceil(page/5),job_loc,tim)
+    num=page%5
+    start=(num-1)*5
+    end=(num)*5 
     return val[start:end]
 
 
@@ -27,17 +28,18 @@ def get_audio_details():
     gen = request.args.get('when_generated', type=str)
     page_num = request.args.get('page_num', type=int)
     details=job_datas.start(job_name,loc,job_type,job_exp,page_num,job_loc,gen)
-    print(details)
-    num=math.floor(page_num/5)
-    start=(num)*5
-    end=(num+1)*5
 
+    # num=math.floor(page_num/5)
+    num=page_num%5
+    start=(num-1)*5
+    end=(num)*5
     # job_data=[]
     # for detail in details:
     #     for job in detail:
     #         job_data.append(job)
     
     # return jsonify(job_data)
+    print(details)
     return jsonify(details[start:end])
 if __name__=="__main__":
     app.run() 
